27750cbc4461ba6403bf191680827a0bbfd8cd20
Tatiana Lipinskaya <lipinskayatatia@gmail.com>
2021-11-23 15:13:58 +0300

    .

--- src/test/java/course/automation/page/FillComputeEngineForm.java
+++ src/test/java/course/automation/page/FillComputeEngineForm.java
@@ -2,7 +2,11 @@ package course.automation.page;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
